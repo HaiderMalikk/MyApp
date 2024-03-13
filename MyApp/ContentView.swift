0f3eea4a6faa7bd09c3c5f011f37373e5dc26de0
HaiderMalikk <haidermalik662@gmail.com>
2024-03-12 23:43:52 -0400

    REcommit

--- MyApp/ContentView.swift
+++ MyApp/ContentView.swift
@@ -7,18 +7,40 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ContentView: View { // CONTENT view is what created the UI and displays it
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world UPDATE")
+        VStack { // v stack can put things on top of each other
+            Link("Follow me on GitHub",
+                  destination: URL(string: "https://github.com/HaiderMalikk")!)
+            .padding(.vertical, 30)
+            // Image element using Image word
+            Image("table")
+                .resizable()
+                .padding(.bottom, 10.0)
+                .frame(width: 350.0, height: 250.0)
+                .cornerRadius(30)
+            Text("Ionizing Energy")
+                .font(.body)
+                .fontWeight(.semibold)
+                .foregroundColor(Color.black)
+                .bold()
+                .padding(.vertical, 10.0)
+            Button("Print in console") {
+                print("Hello World!")
+            }
         }
-        .padding()
+        .padding() // padding on all 4 sides of vstack centers and aligns VStack
     }
 }
 
+// mark keywork makes a selection seetoolbar
+// MARK: - this is for preview
+
+// calling the function to display
 #Preview {
     ContentView()
 }
+
+/*
+ docstring drag breakpoints off
+ */
